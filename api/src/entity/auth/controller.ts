1ee74cbe464f3eba0a1e44097f98b6bc7b452599
Sam Bautista <tadeolinco@gmail.com>
2018-05-06 18:55:39 +0800

    Remove logs

--- api/src/entity/auth/controller.ts
+++ api/src/entity/auth/controller.ts
@@ -30,7 +30,6 @@ const controller = {
         })
         delete user.password
         req.session.user = user
-        console.log(user)
         res.status(200).json({ user })
       } catch (err) {
         console.log(err)
